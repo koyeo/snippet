2ac94a02874a2e31a58cff0fca3d464cabc0a2d8
Koyeo <im@koyeo.io>
2020-04-09 14:42:41 +0800

    edit storage

--- storage/storage.go
+++ storage/storage.go
@@ -6,11 +6,21 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	goPath "path"
 	"path/filepath"
-	"regexp"
 	"strings"
 )
+
+func Abs(path ...string) string {
+	return filepath.Join(strings.TrimPrefix(filepath.Join(path[:]...), Root()))
+}
+
+func Root() string {
+	root, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return root
+}
 
 func Join(paths ...string) string {
 
@@ -23,30 +33,21 @@ func Join(paths ...string) string {
 
 func ReadFile(path string) (content string, err error) {
 
+	if !PathExist(path) {
+		return
+	}
+
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return
 	}
+
 	content = string(data)
 
 	return
 }
 
-func Remove(path string) (err error) {
-	err = os.RemoveAll(path)
-	return
-}
-
-func PathExists(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false
-	} else if err != nil {
-		logger.Fatal("Check file exists:", err)
-	}
-	return true
-}
-
-func Files(path string, suffix ...string) (files []string, err error) {
+func ReadFiles(debug bool, path string, ignore []string, prefix []string, suffix []string) (files []string, err error) {
 
 	dir, err := ioutil.ReadDir(path)
 	if err != nil {
@@ -56,17 +57,35 @@ func Files(path string, suffix ...string) (files []string, err error) {
 	sep := string(os.PathSeparator)
 
 	for _, fi := range dir {
+		path := filepath.Join(path, sep, fi.Name())
+		items := strings.Split(path, sep)
+		subPath := strings.Join(items[1:], sep)
+		var ok bool
+		for _, v := range ignore {
+			ok, err = filepath.Match(v, subPath)
+			if err != nil {
+				logger.Fatal("Match ignore path error: ", err)
+			}
+			if ok {
+				break
+			}
+		}
+		if ok {
+			if debug {
+				logger.IgnoreReadPath("Read files ignore path:", subPath)
+			}
+			continue
+		}
 		if fi.IsDir() {
-			var dirFiles []string
-			dirFiles, err = Files(filepath.Join(path, sep, fi.Name()), suffix...)
+			var items []string
+			items, err = ReadFiles(debug, path, ignore, prefix, suffix, )
 			if err != nil {
 				return
 			}
-			files = append(files, dirFiles...)
+			files = append(files, items...)
 		} else {
-
-			if hasSuffix(suffix, fi.Name()) {
-				files = append(files, filepath.Join(path, sep, fi.Name()))
+			if HasPrefix(fi.Name(), prefix...) || HasSuffix(fi.Name(), suffix...) {
+				files = append(files, path)
 			}
 		}
 	}
@@ -74,9 +93,55 @@ func Files(path string, suffix ...string) (files []string, err error) {
 	return
 }
 
-func hasSuffix(suffix []string, fileName string) bool {
-	for _, v := range suffix {
-		if strings.HasSuffix(fileName, v) {
+func ReadDirs(debug bool, path string, ignore []string, prefix []string, suffix []string) (dirs []string, err error) {
+
+	infos, err := ioutil.ReadDir(path)
+	if err != nil {
+		return
+	}
+
+	sep := string(os.PathSeparator)
+
+	for _, fi := range infos {
+		path := filepath.Join(path, sep, fi.Name())
+		items := strings.Split(path, sep)
+		subPath := strings.Join(items[1:], sep)
+		var ok bool
+		for _, v := range ignore {
+			ok, err = filepath.Match(v, subPath)
+			if err != nil {
+				logger.Fatal("Match ignore path error: ", err)
+			}
+			if ok {
+				break
+			}
+		}
+		if ok {
+			if debug {
+				logger.IgnoreReadPath("Read dirs ignore path:", subPath)
+			}
+			continue
+		}
+
+		if fi.IsDir() {
+			if HasPrefix(fi.Name(), prefix...) || HasSuffix(fi.Name(), suffix...) {
+				dirs = append(dirs, path)
+			}
+			var items []string
+			items, err = ReadDirs(debug, path, ignore, prefix, suffix)
+
+			dirs = append(dirs, items...)
+		}
+	}
+
+	return
+}
+
+func HasPrefix(name string, prefix ...string) bool {
+
+	for _, v := range prefix {
+		v = strings.TrimSpace(v)
+		if v != "" && strings.HasPrefix(name, v) {
 			return true
 		}
 	}
@@ -84,20 +149,20 @@ func hasSuffix(suffix []string, fileName string) bool {
 	return false
 }
 
-func Root() string {
-
-	root, err := os.Getwd()
-	if err != nil {
-		logger.Fatal("Get root path error: ", err)
+func HasSuffix(name string, suffix ...string) bool {
+	for _, v := range suffix {
+		v = strings.TrimSpace(v)
+		if v != "" && strings.HasSuffix(name, v) {
+			return true
+		}
 	}
 
-	return root
+	return false
 }
 
 func MakeDir(path string) {
 
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		// os.Mkdir(path, os.ModePerm)
 		cmd := exec.Command("bash", "-c", fmt.Sprintf("mkdir -p %s", path))
 		_ = cmd.Run()
 		logger.MakeDirSuccess(path)
@@ -105,17 +170,18 @@ func MakeDir(path string) {
 
 }
 
-func MakeDirQuiet(path string) {
-
+func PathExist(path string) bool {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		cmd := exec.Command("bash", "-c", fmt.Sprintf("mkdir -p %s", path))
-		_ = cmd.Run()
+		return false
+	} else if err != nil {
+		logger.Fatal("Check file exists:", err)
 	}
-
+	return true
 }
 
-func Abs(path ...string) string {
-	return filepath.Join(strings.TrimPrefix(filepath.Join(path[:]...), Root()))
+func Remove(path string) (err error) {
+	err = os.RemoveAll(path)
+	return
 }
 
 func WriteFile(path string, content []byte) (err error) {
@@ -123,93 +189,5 @@ func WriteFile(path string, content []byte) (err error) {
 	if err != nil {
 		return
 	}
-	return
-}
-
-func RelativePath(path string) string {
-	wd, _ := os.Getwd()
-	path = strings.TrimPrefix(path, wd+"/")
-	return path
-}
-
-func PackagePath(path string) string {
-	wd, _ := os.Getwd()
-	items := strings.Split(wd, "/")
-
-	if len(items) == 0 {
-		return path
-	}
-
-	return filepath.Join(items[len(items)-1], path)
-}
-
-func ParseFilePath(filePath string) (path, file, ext string) {
-	file = goPath.Base(filePath)
-	path = strings.TrimSuffix(filePath, file)
-	path = strings.TrimRight(path, "/")
-	ext = goPath.Ext(file)
-	file = strings.TrimSuffix(file, ext)
-	ext = strings.TrimLeft(ext, ".")
-	return
-}
-
-func ParseYamlMapLines(content string, fields ...string) (lines []string) {
-	items := strings.Split(content, "\n")
-	if len(fields) == 0 {
-		lines = items
-		return
-	}
-
-	depth := 0
-	expected := len(fields)
-	spacesRegex := regexp.MustCompile(`^(\s+).+\s*$`)
-
-	index := 0
-	field := fmt.Sprintf("%s:", fields[index])
-	lineSpacesCount := 0
-
-	for l, v := range items {
-
-		if depth != expected {
-			if strings.TrimSpace(v) == field {
-				if index < expected-1 {
-					index++
-					field = fmt.Sprintf("%s:", fields[index])
-				}
-
-				depth++
-				if depth == expected {
-					continue
-				}
-			}
-
-		} else {
-
-			strs := spacesRegex.FindStringSubmatch(v)
-
-			if len(strs) != 2 {
-				break
-			}
-
-			if lineSpacesCount == 0 {
-				lineSpacesCount = len(strs[1])
-			}
-
-			if len(strs[1]) != lineSpacesCount {
-				break
-			}
-			lines = append(lines, strings.TrimSpace(v))
-		}
-
-		if depth != 0 {
-			if l+1 < len(items)-1 {
-				strs := spacesRegex.FindStringSubmatch(items[l+1])
-				if len(strs) == 0 {
-					break
-				}
-			}
-		}
-	}
-
 	return
 }
