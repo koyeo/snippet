2194c2a44915c0959a72563b077d5182302a3f6b
Koyeo <im@koyeo.io>
2020-04-10 20:12:12 +0800

    fix new var

--- lang/golang/blocks.go
+++ lang/golang/blocks.go
@@ -9,12 +9,12 @@ func NewConstant(name, value string) *snippet.Block {
 	return snippet.NewBlock(ConstFilter(name), fmt.Sprintf("%s=%s", name, value), nil)
 }
 
-func NewVar(name, value string, data interface{}) *snippet.Block {
-	return snippet.NewBlock(VarFilter(name), fmt.Sprintf("%s=%s", name, value), data)
+func NewVar(name, code string, data interface{}) *snippet.Block {
+	return snippet.NewBlock(VarFilter(name), code, data)
 }
 
-func NewType(name, code string) *snippet.Block {
-	return snippet.NewBlock(TypeFilter(name), code, nil)
+func NewType(name, code string, data interface{}) *snippet.Block {
+	return snippet.NewBlock(TypeFilter(name), code, data)
 }
 
 func NewFunc(name, code string, data interface{}) *snippet.Block {
