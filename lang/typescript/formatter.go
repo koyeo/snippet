48b6331391ea7497e34aae47b3fbfacbbd99ac8f
Koyeo <im@koyeo.io>
2020-05-04 20:25:34 +0800

    edit formartter

--- lang/typescript/formatter.go
+++ lang/typescript/formatter.go
@@ -2,6 +2,7 @@ package typescript
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
 	"strings"
 )
@@ -15,10 +16,12 @@ func Formatter(content string) (output string, err error) {
 	c.Stderr = errBuffer
 	err = c.Start()
 	if err != nil {
+		err = fmt.Errorf(err.Error() + " " + errBuffer.String())
 		return
 	}
 	err = c.Wait()
 	if err != nil {
+		err = fmt.Errorf(err.Error() + " " + errBuffer.String())
 		return
 	}
 	output = outBuffer.String()
